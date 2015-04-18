2c756f169a789d0e7769fcb2a0b0a448a5a640ff
Brian Scaturro <scaturrob@gmail.com>
2015-04-18 14:59:01 -0400

    initial commit

---
+++ dist/main.js
@@ -0,0 +1,39 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+"use strict";
+
+var _interopRequire = function (obj) { return obj && obj.__esModule ? obj["default"] : obj; };
+
+var Klass = _interopRequire(require("./klass"));
+
+var main = new Klass();
+
+console.log(main.render());
+
+},{"./klass":2}],2:[function(require,module,exports){
+"use strict";
+
+var _createClass = (function () { function defineProperties(target, props) { for (var key in props) { var prop = props[key]; prop.configurable = true; if (prop.value) prop.writable = true; } Object.defineProperties(target, props); } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+var _classCallCheck = function (instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } };
+
+var Klass = (function () {
+  function Klass() {
+    _classCallCheck(this, Klass);
+
+    this.name = "Klass";
+  }
+
+  _createClass(Klass, {
+    render: {
+      value: function render() {
+        return "string value";
+      }
+    }
+  });
+
+  return Klass;
+})();
+
+module.exports = Klass;
+
+},{}]},{},[1]);

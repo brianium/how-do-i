c760592fb6d00c026401e6908dec9e7c64ac4968
Brian Scaturro <scaturrob@gmail.com>
2015-04-19 00:32:10 -0400

    embed the first youtube result

--- dist/main.js
+++ dist/main.js
@@ -9,10 +9,17 @@ var app = _interopRequire(require("./app"));
  * Run the application as soon as dom content has loaded
  */
 document.addEventListener("DOMContentLoaded", app.run(function (token) {
-  fetch("https://www.googleapis.com/youtube/v3/search?access_token=" + token + "&part=id,snippet&q=hello", { mode: "cors" }).then(function (resp) {
+  fetch("https://www.googleapis.com/youtube/v3/search?access_token=" + token + "&part=id,snippet&q=hello&type=video&videoEmbeddable=true", { mode: "cors" }).then(function (resp) {
     return resp.text();
   }).then(JSON.parse).then(function (result) {
-    return console.log(result);
+    var item = result.items[0];
+
+    var iframe = document.createElement("iframe");
+    iframe.width = 640;
+    iframe.height = 360;
+    iframe.src = "http://www.youtube.com/v/" + item.id.videoId + "?version=3&enablejsapi=1&autoplay=1";
+
+    document.body.appendChild(iframe);
   });
 }));
 

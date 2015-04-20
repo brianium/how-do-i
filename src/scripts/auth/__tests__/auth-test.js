04b2ce97ddd21677f39e92a6d6795df1d432c14a
Brian Scaturro <scaturrob@gmail.com>
2015-04-19 20:29:04 -0400

    functional auth module

--- src/scripts/auth/__tests__/auth-test.js
+++ src/scripts/auth/__tests__/auth-test.js
@@ -4,17 +4,17 @@ jest.dontMock('../../array');
 jest.dontMock('../../object');
 jest.dontMock('../../monad/maybe');
 
-import auth from '../';
+import {link, parse} from '../';
 
 describe('auth', function () {
   it('can generate an auth link', function () {
-    let link = auth.link('myclient', 'myredirect');
+    let result = link('myclient', 'myredirect');
     let expected = 'https://accounts.google.com/o/oauth2/auth?client_id=myclient&redirect_uri=myredirect&scope=' + encodeURIComponent('https://www.googleapis.com/auth/youtube') + '&response_type=token';
-    expect(link).toEqual(expected);
+    expect(result).toEqual(expected);
   });
 
   it('can parse a fragment for a token', function () {
-    let jwt = auth.parse('#access_token=mytoken&token_type=Bearer&expires_in=3600')
+    let jwt = parse('#access_token=mytoken&token_type=Bearer&expires_in=3600')
     expect(jwt).toEqual({
       access_token:'mytoken',
       token_type: 'Bearer',
@@ -23,6 +23,6 @@ describe('auth', function () {
   });
 
   it('can handle parsing an invalid fragment', function () {
-    expect(auth.parse('whaatttt??')).toBeUndefined();
+    expect(parse('whaatttt??')).toBeUndefined();
   });
 });
--- src/scripts/auth/index.js
+++ src/scripts/auth/index.js
@@ -32,52 +32,46 @@ const matchToJWT = _.flow(
   obj.keySetter('expires_in', parseInt)
 );
 
-let auth = {
+/**
+ * Get the login link
+ *
+ * @param {String} clientID
+ * @param {String} redirectUri
+ * @return {String}
+ */
+export function link(clientId, redirectUri) {
+  let query = {
+    client_id: clientId,
+    redirect_uri: redirectUri,
+    scope: 'https://www.googleapis.com/auth/youtube',
+    response_type: 'token'
+  };
+  let root = 'https://accounts.google.com/o/oauth2/auth?';
+  return root + _.reduce(query, appendParam, '').slice(0, -1);
+}
 
-  /**
-   * Get the login link
-   *
-   * @param {String} clientID
-   * @param {String} redirectUri
-   * @return {String}
-   */
-  link(clientId, redirectUri) {
-    let query = {
-      client_id: clientId,
-      redirect_uri: redirectUri,
-      scope: 'https://www.googleapis.com/auth/youtube',
-      response_type: 'token'
-    };
-    let root = 'https://accounts.google.com/o/oauth2/auth?';
-    return root + _.reduce(query, appendParam, '').slice(0, -1);
-  },
-
-  /**
-   * Get a token from a hash fragment
-   *
-   * @param {String} fragment
-   * @return {Object}
-   */
-  parse(fragment) {
-    let result = TOKEN_PATTERN.exec(fragment);
-    return maybe(result, !!!result)
-      .bind(matchToJWT)
-      .map(jwt => jwt);
-  }
-
-};
+/**
+ * Get a token from a hash fragment
+ *
+ * @param {String} fragment
+ * @return {Object}
+ */
+export function parse(fragment) {
+  let result = TOKEN_PATTERN.exec(fragment);
+  return maybe(result, !!!result)
+    .bind(matchToJWT)
+    .map(jwt => jwt);
+}
 
 /**
  * Get an auth token from a fragment or a cookie
  *
  * @return {String}
  */
-auth.token = _.flow(
-  auth.parse.bind(null, window.location.hash),
+export const token = _.flow(
+  parse.bind(null, window.location.hash),
   token => maybe(token, !!!token).map(jwt => Cookies.set('access_token', jwt.access_token, {
     expires: jwt.expires_in
   })),
   () => Cookies.get('access_token')
 );
-
-export default auth;
